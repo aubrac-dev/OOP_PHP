52a6829e0d885b660ab5b2478191f9314f4cb42c
Ioan Ovidiu Toma <87045064+aubrac-dev@users.noreply.github.com>
2021-10-27 21:59:00 +0200

    les propriétés et méthodes statiques

--- index.php
+++ index.php
@@ -4,61 +4,80 @@ declare(strict_types=1);
 
 class Pont
 {
-    private const SURFACE_TEXT = 'Ce pont mesure %d m².';
-    // private string $unite = ' m²';
+    // ous avons renommé $unite en UNITE : les constantes ne sont pas préfixées par $ et par convention sont écrites en UPPER_SNAKE_CAS
+    private const UNITE = 'm²';  // constante de classe s’écrit avec le mot-clé const est une valeur lui est donnée directement
     private float $longueur;
     private float $largeur;
 
-    // appliquer le principe d’encapsulation avec set et get
-    public function setLongueur(float $longueur): void  // setter / mutateur
+    public function getSurface(): string
     {
-        if ($longueur < 0) {
+        // Une constante est statique, on utilise le mot-clé self pour l’utiliser
+        return ($this->longueur * $this->largeur) . self::UNITE;
+    }
+
+    // setLongueur et setLargeur ne changent pas
+
+    /*Pour utiliser une méthode sans instance, elle doit être déclarée statique 
+    et nous ferons attention de manipuler uniquement des propriétés statiques.
+    Lorsqu’une propriété est déclarée statique, la valeur qu’elle contient 
+    sera partagée pour toutes les instances.*/
+
+    public static function validerTaille(float $taille): bool
+    {
+        if ($taille < 50.0) {
             trigger_error(
-                'La longueur est trop courte. (min 1)',
+                'La longueur est trop courte. (min 50m)',
                 E_USER_ERROR
             );
         }
 
+        return true;
+    }
+
+    public function setLongueur(float $longueur): void
+    {
+        self::validerTaille($longueur); // Vous devez utiliser le mot clé self pour cibler une méthode statique 
+        // de classe, lorsque vous l'appelez depuis une instance de cette même classe.
         $this->longueur = $longueur;
     }
 
-    public function setLargeur(float $largeur): void  // setter / mutateur
-    {
-        if ($largeur < 0) {
-            trigger_error(
-                'La longueur est trop courte. (min 1)',
-                E_USER_ERROR
-            );
-        }
+    // self - utilite
 
-        $this->largeur = $largeur;
-    }
+    // Définition de la propriété statique. Elle sera partagée
+    public static int $nombrePietons = 0;
 
-    public function getLongueur(): float   // getter / accesseur
+    // Je laisse volontairement la méthode non statique.
+    // Seule la référence à la propriété est importante.
+    public function nouveauPieton()
     {
-        return $this->longueur;
-    }
-
-    public function getLargeur(): float // getter / accesseur
-    {
-        return $this->largeur;
-    }
-
-    public function getSurface(): string
-    {
-        return ($this->longueur * $this->largeur) . $this->unite;
-    }
-
-    public function printSurface(): void
-    {
-        echo sprintf(self::SURFACE_TEXT, $this->getSurface());
+        // Mise à jour de la propriété statique.
+        self::$nombrePietons++;
     }
 }
 
+var_dump(Pont::validerTaille(150.0));
+//var_dump(Pont::validerTaille(20.0));
+
+/*
+Pont fait référence à la classe or, rappelez-vous, -> permet d'accéder aux éléments d'un objet, 
+c'est-à-dire d'une instance. 
+
+Pour dire à PHP que nous souhaitons faire référence à un élément de la classe, 
+il faut utiliser ::  à la place, comme pour les constantes. Sans ceci, il va chercher 
+à utiliser la méthode d'un objet contenu dans une constante nommée Pont. Ce qui est faux dans notre cas.
+*/
+
 $towerBridge = new Pont;
-$towerBridge->setLongueur(500);
-$towerBridge->setLargeur(80);
+//$towerBridge->setLongueur(20.0);
 
-$towerBridge->printSurface();
+// ----------------------------------------------
+//En définissant une propriété statique, vous allez pouvoir manipuler
+// une même donnée à travers toutes les instances.
 
-// echo 'Pont en surface de ' . $towerBridge->getSurface() . ' pour longueur de ' . $towerBridge->getLongueur() . ' m et largeur de ' . $towerBridge->getLargeur() . ' m.';
+$pontLondres = new Pont;
+$pontLondres->nouveauPieton();
+
+$pontManhattan = new Pont;
+$pontManhattan->nouveauPieton();
+
+echo Pont::$nombrePietons;
