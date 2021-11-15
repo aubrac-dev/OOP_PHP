963315b7fd535ce54e6fa8aa0583de2c2fde9ad4
Ioan Ovidiu Toma <87045064+aubrac-dev@users.noreply.github.com>
2021-11-15 21:20:38 +0100

    1-2-3

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ class User
     public const STATUS_ACTIVE = 'active';
     public const STATUS_INACTIVE = 'inactive';
 
+    public static $nombreUtilisateursInitialisés = 0;
+
     public string $username;
     public string $status;
 
@@ -32,11 +34,20 @@ class User
     {
         return $this->status;
     }
+
+    public function printStatus()
+    {
+        echo $this->status;
+    }
 }
 
 class Admin extends User
 {
+    public static $nombreAdminInitialisés = 0;
+
     public array $roles;
+
+    public const STATUS_LOCKED = 'locked';
 
     // Ajout d'un tableau de roles pour affiner les droits des administrateurs :)
     public function __construct(string $username, array $roles = [], string $status = self::STATUS_ACTIVE)
@@ -64,36 +75,62 @@ class Admin extends User
     {
         $this->roles = $roles;
     }
+
+    /* public function printStatus()
+    {
+        // vous pouvez accéder au statut comme si la propriété appartenait à Admin :)
+        echo $this->status;
+    }*/
+
+    // mise à jour des valeurs des propriétés statiques de la classe courante avec `self`, et de la classe parente avec `parent`
+    public static function nouvelleInitialisation()
+    {
+        self::$nombreAdminInitialisés++; // classe Admin
+        parent::$nombreUtilisateursInitialisés++; // classe User
+    }
+
+    public function printCustomStatus()
+    {
+        echo "L'administrateur {$this->username} a pour statut : ";
+        $this->printStatus(); // on appelle printStatus du parent depuis la classe enfant
+    }
+
+    // la méthode est entièrement réécrite ici :) seule la signature reste inchangée
+    public function setStatus(string $status): void
+    {
+        if (!in_array($status, [self::STATUS_ACTIVE, self::STATUS_INACTIVE, self::STATUS_LOCKED])) {
+            trigger_error(sprintf(
+                'Le status %s n\'est pas valide. Les status possibles sont : %s',
+                $status,
+                implode(', ', [self::STATUS_ACTIVE, self::STATUS_INACTIVE, self::STATUS_LOCKED])
+            ), E_USER_ERROR);
+        };
+
+        $this->status = $status;
+    }
+
+    // la méthode utilisée est celle de la classe parente, puis ajoute un comportement :)
+    public function getStatus(): string
+    {
+        return strtoupper(parent::getStatus());
+    }
 }
 
-$rl = [];
-$rl[] = 'MASTER';
-$admin1 = new Admin('root');
-//var_dump($admin1);
-$admin1->addRole('GROUP');
-$admin1->addRole('SUPERVISOR');
-$admin1->addRole('GROUP');
-$admin1->setStatus(User::STATUS_INACTIVE);
-print_r($admin1->getRoles());
-var_dump($admin1);
-$admin1->setRoles($rl);
-$admin1->setStatus(User::STATUS_ACTIVE);
-print_r($admin1->getRoles());
-var_dump($admin1);
+/*
+$admin = new Admin('Maria');
+$admin->printStatus();
 
+Admin::nouvelleInitialisation();
+var_dump(Admin::$nombreAdminInitialisés, Admin::$nombreUtilisateursInitialisés, User::$nombreUtilisateursInitialisés);
+// var_dump(User::$nombreAdminInitialisés); // ceci ne marche pas.
+*/
 
 /*
-// array 
- $roles = [];
- print_r($roles);
- $roles [] = 'a';
- print_r($roles);
- $roles [] = 'b';
- print_r($roles);
- $roles [] = 'b';
- print_r($roles);
- $roles [] = 'c';
- print_r($roles);
- $roles [] = array_unique($roles);
- print_r($roles);
+$admin = new Admin('Lily');
+$admin->printCustomStatus(); // Affiche “L’administrateur Lily a pour statut : active”
+$admin->printStatus(); // printStatus n’existe pas dans la classe Admin, donc printStatus de la classe User sera appelée grâce à l’héritage
 */
+
+$admin = new Admin('Paddington');
+$admin->setStatus(Admin::STATUS_LOCKED);
+echo $admin->getStatus();
