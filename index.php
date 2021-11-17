eb20437c2e876019fae8a81210efd7706478221f
Ioan Ovidiu Toma <87045064+aubrac-dev@users.noreply.github.com>
2021-11-17 08:33:57 +0100

    abstract - final

--- index.php
+++ index.php
@@ -2,59 +2,84 @@
 
 declare(strict_types=1);
 
-class User
+abstract class User //Imposez l'héritage d’une classe
 {
-    protected const STATUS_ACTIVE = 'active';
-    protected const STATUS_INACTIVE = 'inactive';
+    public const STATUS_ACTIVE = 'active';
+    public const STATUS_INACTIVE = 'inactive';
 
-    protected string $username;
-    protected string $status;
+    public string $email;
+    public string $status;
 
-    public function __construct(string $username,  string $status = self::STATUS_ACTIVE)
+    public function __construct(string $email, string $status = self::STATUS_ACTIVE)
     {
-        $this->username = $username;
+        $this->email = $email;
         $this->status = $status;
     }
 
-    protected function setStatus(string $status): void
-    {
-        assert(
-            in_array($status, [self::STATUS_ACTIVE, self::STATUS_INACTIVE]),
-            sprintf('Le status %s n\'est pas valide. Les status possibles sont : %s', $status, implode(', ', [self::STATUS_ACTIVE, self::STATUS_INACTIVE]))
-        );
-
-        $this->status = $status;
-    }
-
-    protected function getStatus(): string
-    {
-        return $this->status;
-    }
-}
-
-class Admin extends User
-{
-    public const STATUS_LOCKED = 'locked';
-
-    // la méthode est entièrement ré-écrite ici :) seule la signature reste inchangée
     public function setStatus(string $status): void
     {
         assert(
-            in_array($status, [self::STATUS_ACTIVE, self::STATUS_INACTIVE, self::STATUS_LOCKED]),
-            sprintf('Le status %s n\'est pas valide. Les status possibles sont : %s', $status, implode(', ', [self::STATUS_ACTIVE, self::STATUS_INACTIVE, self::STATUS_LOCKED]))
+            in_array($status, [self::STATUS_ACTIVE, self::STATUS_INACTIVE]),
+            sprintf(
+                'Le status %s n\'est pas valide. Les status possibles sont : %s',
+                $status,
+                implode(',', [self::STATUS_ACTIVE, self::STATUS_INACTIVE])
+            )
         );
 
         $this->status = $status;
     }
 
-    // la méthode utilise celle de la classe parente, et ajoute un comportement :)
     public function getStatus(): string
     {
-        return strtoupper(parent::getStatus());
+        return $this->status;
+    }
+
+    // cette méthode doit exister dans les classes enfants, sans dire comment elle fonctionne
+    abstract public function getUsername(): string;
+}
+
+final class Admin extends User // Empêchez l'héritage
+{
+
+    public $roles = [];
+
+    // Ajout d'un tableau de roles pour affiner les droits des administrateurs :)
+    public function __construct(string $email, string $status = self::STATUS_ACTIVE, array $roles = [])
+    {
+        parent::__construct($email, $status);
+        $this->roles = $roles;
+    }
+
+    // ...
+
+    public function getUsername(): string
+    {
+        return $this->email;
     }
 }
 
+class Player extends User
+{
+    public string $username;
+    // Ajout username :)
+    public function __construct(string $email, string $username, string $status = self::STATUS_ACTIVE)
+    {
+        parent::__construct($email, $status);
+        $this->username = $username;
+    }
 
-$admin = new Admin('Paddington');
-$admin->setStatus(Admin::STATUS_LOCKED);
-echo $admin->getStatus();
+    // ...
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+}
+
+$admin = new Admin('root@admin.net');
+//var_dump($admin);
+//echo $admin->getUsername();
+$player = new Player('player@admin.net', 'MyPlayer');
+//var_dump($player);
+echo $player->getUsername();
